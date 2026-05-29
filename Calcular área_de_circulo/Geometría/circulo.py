0f9a2dbd297353b8cafe58f66dd4decbe2d8e3bd
Jairon <jaironcoronadom@gmail.com>
2026-05-28 22:56:34 -0600

    Calculate the area of ​​a circle (Solved)
    I create a folder with name(), inside that folder I create a file that contains the main function to calculate the area, outside that folder we create a file that is our main main, we call it from our main main and it delivers the area of ​​the circle already solved.

---
+++ Calcular área_de_circulo/Geometría/circulo.py
@@ -0,0 +1,6 @@
+# Función para calcular el área de un círculo
+
+def calcular_area_circulo(radio):
+    pi = 3.14159
+    area = pi * (radio ** 2)
+    return f"{area:.2f}"
---
+++ Calcular área_de_circulo/main.py
@@ -0,0 +1,7 @@
+# Programa principal para calcular el área de un círculo
+from Geometría.circulo import calcular_area_circulo
+
+radio = 8
+area = calcular_area_circulo(radio)
+
+print(f"El área del círculo es: {area}")
