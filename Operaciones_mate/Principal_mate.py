07c21d48c35c452059516ef8234d653889cd0120
Jairon <jaironcoronadom@gmail.com>
2026-05-28 22:49:53 -0600

    Result of basic mathematical operations
    I create a module with basic math functions and import it into our main file.

---
+++ Operaciones_mate/Principal_mate.py
@@ -0,0 +1,9 @@
+# Este es el archivo principal que importa la clase Matematica y utiliza sus métodos para realizar operaciones matemáticas.
+# Resultados de las operaciones se imprimen a continuación.
+import matematicas as mat
+
+print("Suma:", mat.sumar(4, 8))
+print("Resta:", mat.restar(10, 3))
+print("Multiplicación:", mat.multiplicar(5, 6))
+print("División:", mat.dividir(15, 3))
+print("División por cero:", mat.dividir(10, 0))
---
+++ Operaciones_mate/matematicas.py
@@ -0,0 +1,18 @@
+# Creación de operaciones Matemáticas básicas
+
+# funciones para sumar, restar, multiplicar y dividir
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: No se puede dividir por cero"
