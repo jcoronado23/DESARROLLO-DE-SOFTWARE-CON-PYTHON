bd436252659af7d54aa25df1f78c466c4438f650
Jairon <jaironcoronadom@gmail.com>
2026-06-10 22:55:44 -0600

    Creating user data (Correcting errors)
    This program allows you to add new users to a database, call users, update their data, and delete their information. All through a menu.

--- Modulo_Usuario/RepositorioUsuario.py
+++ Modulo_Usuario/RepositorioUsuario.py
@@ -1,59 +1,71 @@
+from datetime import datetime, date
 import Usuario
+import random
 
 usuarios = []
 
-#Mantenimiento de un CRUD (Create, Read, Update, Delete) de usuarios
+#Mantenimiento de un CRUD (Create, Read, Update, Delete)
+
+#CREATE
 def crearUsuario():
-    
-    cedula = input("Ingrese la cedula del usuario: ")
-    nombre = input("Ingrese el nombre del usuario: ")
-    
+
+    cedula = random.randint(100000000, 899999999)
+
     for usuario in usuarios:
         if usuario.cedula == cedula:
             print("Esta cedula ya esta en el registro")
             return
-        
-    nuevoUsuario = Usuario(cedula, nombre)
-    usuarios.append(nuevoUsuario)
-    
+
+    nombre = input("Ingrese su nombre: ")
+
+    anno = int(input("Ingrese su anno de nacimiento (dd): "))
+    mes = int(input("Ingrese su mes de nacimiento (mm): "))
+    dia = int(input("Ingrese su dia de nacimiento (yyyy): "))
+
+    nuevo_usuario = Usuario.Usuario(cedula, nombre, anno, mes, dia )
+    usuarios.append(nuevo_usuario)  
+
     print("Usuario agregado satisfactoriamente")
-    
-    
+
+#READ 
 def leerUsuarios():
+    
     if len(usuarios) == 0:
-        print("No hay usuarios sitema")
+        print("No hay usuarios en el sistema")
         return
     
     for usuario in usuarios:
         usuario.mostrarDatos()
-        
-def buscarUsuario(Cedula):
+    
+def buscarUsuario(cedula):
     for usuario in usuarios:
-        if usuario.cedula == Cedula:
+        if usuario.cedula == cedula:
             return usuario
-        
+    
     return None
 
 #UPDATE
 def actualizarUsuario():
-    
+
     cedula = input("Digite su cedula: ")
     usuario = buscarUsuario(cedula)
-    
+
     if usuario:
-        nombre = input("Digite el nuevo nombre: ")
-        
+        nombre = input("Digite su nuevo nombre: ")
+
         usuario.nombre = nombre
-        print("Usuario actualizado satisfactoriamente")
+        print("Se ha actualizado con exito")
+
     else:
-        print("No se ha encontrado el usuario que quiere modificar")
-        
+        print("No se ha encontrado el usuario que quiere modificar.")
+
 def eliminarUsuario():
     cedula = input("Digite su cedula: ")
     usuario = buscarUsuario(cedula)
-    
+
     if usuario:
         usuarios.remove(usuario)
-        print("Usuario eliminado satisfactoriamente")
+        print("Se ha eliminado con exito")
+
     else:
-        print("No se ha encontrado el usuario que quiere eliminar")
+        print("No se ha encontrado el usuario que quiere eliminar.")
--- Modulo_Usuario/Usuario.py
+++ Modulo_Usuario/Usuario.py
@@ -1,8 +1,22 @@
+from datetime import datetime
+
 class Usuario:
-    
-    def __init__(self, cedula, nombre):
+
+    def __init__(self, cedula, nombre, anno_nacimiento, mes_nacimiento, dia_nacimiento):
         self.cedula = cedula
         self.nombre = nombre
-        
+        self.anno_nacimiento = anno_nacimiento
+        self.mes_nacimiento = mes_nacimiento
+        self.dia_nacimiento = dia_nacimiento    
+
     def mostrarDatos(self):
-        print(f"Cedula: {self.cedula}, y nombre: {self.nombre}")
+
+        fecha_nacimiento = datetime(self.anno_nacimiento, self.mes_nacimiento, self.dia_nacimiento)
+
+        hoy = datetime.now()
+
+        edad = hoy.year - fecha_nacimiento.year
+        if( hoy.month, hoy.day ) < (fecha_nacimiento.month, fecha_nacimiento.day):
+            edad -= 1
+
+        print(f"Cedula: {self.cedula}, y nombre: {self.nombre}, y su edad es {edad}")
