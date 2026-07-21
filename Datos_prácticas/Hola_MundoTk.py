d4fe35c65acfb0c1f7a13da2161c44574506d0ba
Jairon <jaironcoronadom@gmail.com>
2026-07-20 22:33:32 -0600

    Practice with Tkinter
    This program is a simple application developed in Python using Tkinter. It creates a window with a counter initialized to zero and two buttons that allow the value to be increased by 5 or 10 units. To achieve this, it uses the `crear_sumador(valor)` function, which generates internal functions capable of modifying the counter and updating the text displayed on the interface's label.

--- Datos_prácticas/Hola_MundoTk.py
+++ Datos_prácticas/Hola_MundoTk.py
@@ -1,22 +1,24 @@
 import tkinter as tk
 
+#Crear una ventana, definir su tamaño y título
 ventana = tk.Tk()
 ventana.title("Mi Primera app")
 ventana.geometry("400x220")
 
 contador = 0
-
-lbl = tk.Label(ventana, text="0", font=("Arial, 20"))
+# lbl para mostrar el valor del contador
+lbl = tk.Label(ventana, text="0", font=("Arial", 20))
 lbl.pack()
 
-
+# Funcion para crear un sumador que sume un valor específico al contador
 def crear_sumador(valor):
     def sumar():
         global contador
         contador += valor
         lbl.config(text=str(contador))
     return sumar
-    
+
+# Botones para sumar 5 y 10 al contador
 tk.Button(ventana, text="suma 5", command=crear_sumador(5)).pack()
 tk.Button(ventana, text="suma 10", command=crear_sumador(10)).pack()
 
