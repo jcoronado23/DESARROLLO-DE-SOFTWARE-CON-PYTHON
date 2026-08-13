e5a7bcab2b0d3c82143c2c85cec84d945b2cd491
Jairon Martinez Coronado <jaironcoronadom@gmail.com>
2026-08-12 20:19:19 -0600

    Actualización de ventana para ingresar credenciales

--- Modulo_Usuario/VentanaLogin.py
+++ Modulo_Usuario/VentanaLogin.py
@@ -6,13 +6,52 @@ from InterfazUsuarios import VentanaUsuarios
 class VentanaLogin:
     
     def __init__(self, raiz):
-        pass
+        self.raiz = raiz
+        self.raiz.title("Iniciar Sesión")
+        self.raiz.geometry("300x220")
+        self.raiz.resizable(False, False)
+        
+        
+        self.crearWidget()
+        
     
     def crearWidget(self):
-        pass
+        tk.Label(self.raiz, text="Usuario:").pack(pady=(30,0))
+        self.entry_usuario = tk.Entry(self.raiz)
+        self.entry_usuario.pack()
+        
+        
+        tk.Label(self.raiz, text="Contraseña:").pack(pady=(15,0))
+        self.entry_contraseña = tk.Entry(self.raiz, show="*")
+        self.entry_contraseña.pack()
+        self.entry_contraseña.bind("<Return>", lambda evento: self.iniciarSecion())
+        
+        tk.Button(self.raiz, text="Ingresar", command=self.iniciarSecion).pack(pady=25)
+        
+        self.entry_usuario.focus_set() 
     
     def iniciarSecion(self):
-        pass
-    
+        usuario = self.entry_usuario.get().strip()
+        contraseña = self.entry_contraseña.get().strip()
+        
+        if usuario == "" or contraseña == "":
+            messagebox.showwarning("Atencion", "Debe completar usuario y contrasenna") 
+            return
+        
+        if RepositorioCredenciales.verificarCredenciales(usuario, contraseña):
+            self.abrirVentanaPrincipal()
+            
+        else:
+            messagebox.showerror("Error", "Usuario o contrasena incorrectos")
+            self.entry_contraseña.delete(0, tk.END)
+            
+            
     def abrirVentanaPrincipal(self):
-        pass
+        for widget in self.raiz.winfo_children():
+            widget.destroy()
+            
+        VentanaUsuarios(self.raiz)
+    
+raiz = tk.Tk()
+app = VentanaLogin(raiz)
+raiz.mainloop()
